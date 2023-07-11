fe973bf5fd5e95e7716f5e8195fb1a25d3820026
Nuwan <nuwan199044@gmail.com>
2023-07-11 15:08:26 +0530

    initial commit

---
+++ src/main/java/com/myapp/springsecurity/version1/HomeResource.java
@@ -0,0 +1,14 @@
+package com.myapp.springsecurity.version1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+
+}
