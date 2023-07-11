a4ed0436fa9e033aba63cc0d26c7c896912c1290
Nuwan <nuwan199044@gmail.com>
2023-07-11 23:33:52 +0530

    adding authorization based on user roles

--- src/main/java/com/myapp/springsecurity/version1/SecurityConfiguration.java
+++ src/main/java/com/myapp/springsecurity/version1/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.myapp.springsecurity.version1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,6 +21,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("test")
                 .password("test")
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/**").permitAll()
+                .and().formLogin();
     }
 
     @Bean
